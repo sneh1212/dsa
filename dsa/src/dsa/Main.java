e0cd4fa4cce448d6491b3c89a36b2bb35cc3f181
sneh1212 <kumarsnehshahu@gmail.com>
2020-11-23 12:04:01 +0530

    2

--- dsa/src/dsa/Main.java
+++ dsa/src/dsa/Main.java
@@ -5,24 +5,10 @@ public class Main {
 	public static void main (String[] args) throws java.lang.Exception
 	{
              
-		Scanner s = new Scanner(System.in);
-		char c[] = s.nextLine().toCharArray();
-		int freq[]=new int[256];
-		for(char i:c)
-		{
-			if(i==' ')
-				continue;
-			freq[i-'a']++;
-		}
-		for(int i=0;i<26;i++)
-		{
-			if(freq[i]>1)
-			{
-				System.out.print((char)(i+'a')+" "+freq[i]);
-				System.out.println();
-			}
-		}
 		
+		
+		// new 
+		System.out.println("good");
 		
      
     }
